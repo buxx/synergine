5e566544896c19453555aa59ea07dccaea37f83d
Bastien Sevajol <bastien.sevajol@xsalto.com>
2015-02-06 14:45:11 +0100

    improve style

--- modules/lifegame/test/src/LifeGameCollectionConfiguration.py
+++ modules/lifegame/test/src/LifeGameCollectionConfiguration.py
@@ -1,34 +1,5 @@
-from synergine.synergy.collection.Configuration import Configuration
-from lifegame.synergy.object.Cell import Cell
-from synergine.metas import metas
-from lifegame.synergy.LifeGameSimulation import LifeGameSimulation
+from lifegame.synergy.collection.LifeGameCollectionConfiguration import \
+     LifeGameCollectionConfiguration as BaseLifeGameCollectionConfiguration
 
-
-class LifeGameCollectionConfiguration(Configuration):
-
-    def get_start_objects(self):
-        cells = []
-        # cell grid
-        for x in range(40):
-            for y in range(50):
-                cell = Cell()
-                cell.add_trace((0, x, y))
-                cells.append(cell)
-
-        alive_cell_traces = (
-            (0, 20, 20),
-            (0, 21, 20),
-            (0, 22, 20),
-            (0, 22, 21),
-            (0, 22, 22),
-            (0, 21, 22),
-            (0, 20, 22)
-        )
-        for dead_cell in cells:
-            if dead_cell.get_point() in alive_cell_traces:
-                dead_cell.set_alive(True)
-                metas.list.add(LifeGameSimulation.STATE, dead_cell.get_id(), LifeGameSimulation.ALIVE)
-            else:
-                metas.list.add(LifeGameSimulation.STATE, dead_cell.get_id(), LifeGameSimulation.DIED)
-
-        return cells
+class LifeGameCollectionConfiguration(BaseLifeGameCollectionConfiguration):
+    pass
