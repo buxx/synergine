0779cb8f5bf3d9ce9697ad3bdbb48abb98129be7
Bastien Sevajol <sevajol.bastien@gmail.com>
2015-04-03 07:57:14 +0200

    meta update when movement

--- modules/xyzworld/SynergyObject.py
+++ modules/xyzworld/SynergyObject.py
@@ -1,3 +1,4 @@
+from intelligine.cst import PREVIOUS_DIRECTION, BLOCKED_SINCE
 from synergine.synergy.object.SynergyObject import SynergyObject as BaseSynergyObject
 from xyzworld.cst import POSITION, POSITIONS
 
@@ -8,12 +9,17 @@ class SynergyObject(BaseSynergyObject):
         super().__init__(collection, context)
         self._position = None
         self._start_position = None
+        self._previous_direction = None
 
     def set_position(self, point):
         if self._position:
             self._context.metas.list.remove(POSITIONS, self._position, self.get_id())
         else:
             self._start_position = point
+
+        if point != self._position:
+            self._context.metas.value.set(BLOCKED_SINCE, self.get_id(), 0)
+
         self._position = point
         self._context.metas.value.set(POSITION, self.get_id(), point)
         self._context.metas.list.add(POSITIONS, point, self.get_id())
@@ -23,3 +29,10 @@ class SynergyObject(BaseSynergyObject):
         :return: (z, x, y)
         """
         return self._position
+
+    def set_previous_direction(self, previous_direction):
+        self._previous_direction = previous_direction
+        self._context.metas.value.set(PREVIOUS_DIRECTION, self.get_id(), previous_direction)
+
+    def get_previous_direction(self):
+        return self._previous_direction
