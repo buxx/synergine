0aeca118cd40acb13ce8e724ed5f5aad6879cee6
Bastien Sevajol <sevajol.bastien@gmail.com>
2015-04-27 18:26:40 +0200

    remove traveller config

--- config.py
+++ config.py
@@ -6,12 +6,6 @@ from lifegame.display.pygame_visualisation import visualisation as pygame_visual
 from xyworld.display.CursesDisplay import CursesDisplay
 from xyworld.display.PygameDisplay import PygameDisplay
 from synergine.display.TestDisplay import TestDisplay
-from traveller.synergy.TravellerSimulation import TravellerSimulation
-from traveller.synergy.TravellerCollection import TravellerCollection
-from traveller.synergy.TravellerCollectionConfiguration import TravellerCollectionConfiguration
-from traveller.display.visualisation import visualisation as traveller_visualisation
-from traveller.display.TravellerDisplay import TravellerDisplay
-from traveller.core.Context import Context as TravellerContext
 from xyzworld.Context import Context as XyzContext
 
 config = {
@@ -60,42 +54,6 @@ config = {
             'display': {
                 'grid': {
                     'size': 1
-                }
-            }
-        }
-    }
-}
-
-config_traveller = {
-    'app': {
-        'name': 'Traveller',
-        'classes': {
-            'Context': TravellerContext
-        }
-    },
-    'engine': {
-        'fpsmax': 255,
-        'debug': {
-            'mainprocess': True,
-            'cycles': -1
-        }
-    },
-    'simulations' : [TravellerSimulation([TravellerCollection(TravellerCollectionConfiguration())])],
-    'connections': [TravellerDisplay],
-    'terminal': {
-        'pygame': {
-            'visualisation': traveller_visualisation,
-            'window_size': (800, 600),
-            'app': {
-                'name': 'Synergine (pygame)'
-            },
-            'font': {
-                'name': 'arial',
-                'size': 13
-            },
-            'display': {
-                'grid': {
-                    'size': 20
                 }
             }
         }
