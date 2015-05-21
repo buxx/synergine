f1bf8f0380cf43e5f4f7fad76a96a6c4134fed84
Bastien Sevajol <sevajol.bastien@gmail.com>
2015-05-21 14:11:00 +0200

    xyz is gone

--- tests/TestSuite.py
+++ tests/TestSuite.py
@@ -2,11 +2,10 @@ from synergine.test.TestSuite import TestSuite as BaseTestSuite
 from tests.simulation.TestSimulation import TestSimulation
 from tests.core.connection.TestTerminal import TestTerminal
 from tests.core.TestActionManager import TestActionManager
-from tests.core.display.TestZone import TestZone
 from tests.core.TestConfigurationManager import TestConfigurationManager
 
 class TestSuite(BaseTestSuite):
 
     def __init__(self):
         super().__init__()
-        self.add_test_cases([TestConfigurationManager, TestZone, TestActionManager, TestSimulation, TestTerminal])
+        self.add_test_cases([TestConfigurationManager, TestActionManager, TestSimulation, TestTerminal])
