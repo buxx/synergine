e7577da065fad7df799bb37ccf5fee59e6891981
Bastien Sevajol <sevajol.bastien@gmail.com>
2015-06-05 15:09:55 +0200

    rebuild

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except (IOError, ImportError):
 
 setup(
     name='synergine',
-    version='0.0.1.4',
+    version='0.0.1.5',
     packages=find_packages(),
     install_requires=[],
     author='Bastien Sevajol',
