b7351f2bf3f91bc7b30095d091073013a2814cb4
Bastien Sevajol <sevajol.bastien@gmail.com>
2015-06-15 22:31:06 +0200

    new release

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except (IOError, ImportError):
 
 setup(
     name='synergine',
-    version='0.0.1.7',
+    version='0.0.1.8',
     packages=find_packages(),
     install_requires=[],
     author='Bastien Sevajol',
